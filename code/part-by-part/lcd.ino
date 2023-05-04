3b9f1543c2cfbb41c1501345124830c6ab40d4f6
Hoshiyo <58986671+hoshiyoff@users.noreply.github.com>
2023-05-04 20:32:27 +0200

    Mise en forme

---
+++ code/part-by-part/lcd.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Projet Bts");
+  lcd.setCursor(2, 1);
+  lcd.print("2022-2023");
+
+}
